572b6acbd3c947aa2649445108b8589f8dd6fb80
japsolo <javier@adboxpublicidad.com>
2018-07-30 19:58:21 -0300

    correcciones

--- src/templates/App.js
+++ src/templates/App.js
@@ -1,13 +1,36 @@
+// 1er ERROR
+// Estaba mal la ruta en el package.json que buscaba al archivi App.js
+// Tenías = "main": "./templates/App.js"
+// Y es = "main": "./src/templates/App.js"
 
+// 2do ERROR
+// Tenés que requerir ('http')
+// Tenés que requerir ('fs')
+
+// 3er ERROR
+// Tenés mal la ruta a cada archivo en el método readFileSync
+// El path correcto sería __dirname + "/libros.html"
+// Esto es así porque el archivo App.js está en la misma carpeta
+
+// 4to ERROR
+// Tenés mal la ruta a bootstrap en el HTML y a su vez el Content-Type
+// El Content-Type es 'text/css'
+// Chusmeate el libros.html y fijate como hice para llamar a bootstrap en el <link>
+
+const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
 	switch (req.url) {
-		case "/libros":
+		case "/":
 			res.writeHead(200, { "Content-Type": "text/html" });
-			var html = fs.readFileSync(__dirname + "/template/libros.html", "utf8");
+			var html = fs.readFileSync(__dirname + "/index.html", "utf8");
 			res.end(html);
 		break;
-
+			res.writeHead(200, { "Content-Type": "text/html" });
+			var html = fs.readFileSync(__dirname + "/libros.html", "utf8");
+			res.end(html);
+		break;
 		case "/discos":
 			res.writeHead(200, { "Content-Type": "text/html" });
 			var html = fs.readFileSync(__dirname + "/template/discos.html", "utf8");
@@ -20,13 +43,13 @@ http.createServer((req, res) => {
 		break;
 		case "/verTodo":
 			res.writeHead(200, { "Content-Type": "text/html" });
-			var html = fs.readFileSync(__dirname + "/template/verTodo.html", "utf8");
+			var html = fs.readFileSync(__dirname + "/verTodo.html", "utf8");
 			res.end(html);
 		break;
 		case "/bootstrap":
-			res.writeHead(200, { "Content-Type": "text/html" });
-			var cssBootstrap = fs.readFileSync(__dirname + "/templates/css/bootstrap.min.css", "utf8");
-			res.end(cssStyles);
+			res.writeHead(200, { "Content-Type": "text/css" });
+			var cssBootstrap = fs.readFileSync(__dirname + "/css/bootstrap.min.css", "utf8");
+			res.end(cssBootstrap);
 		break;
 		default:
 			res.writeHead(400, { "Content-Type": "text/plain" });
@@ -79,7 +102,7 @@ http.createServer((req, res)=>{
 
 /*
 jap_solo [13:00]
-Sin título 
+Sin título
 http.createServer((req, res) => {
 	switch (req.url) {
 		case '/':
